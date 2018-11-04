95772d124553673a7154f4bd435c6f15415bf49c
37bird <39220220+37bird@users.noreply.github.com>
2018-11-04 17:12:18 +0000

    The solution added
    ta daa.

---
+++ index.js
@@ -0,0 +1,29 @@
+let notTheNumber = true;
+
+    for(let i = 1; notTheNumber = true; i++){
+
+        if(i/20 %1) continue; {
+            if(i/19 %1) continue; {
+                if(i/18 %1) continue; {
+                    if(i/17 %1) continue; {
+                        if(i/16 %1) continue; {
+                            if(i/15 %1) continue; {
+                                if(i/14 %1) continue; {
+                                    if(i/13 %1) continue; {
+                                        if(i/12 %1) continue; {
+                                            if(i/11 %1) continue; {
+                                                console.log('Number found: ' + i);
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
